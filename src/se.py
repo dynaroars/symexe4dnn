60e99f7640e841123c64ae8ba270bf63f78b55c0
lli <lmax.qaz@gmail.com>
2023-04-01 23:50:36 -0400

    duh

--- src/se.py
+++ src/se.py
@@ -78,7 +78,7 @@ def symbolic_execution(dnn):
             else:
                 symbolic_states.append(aRefs[nName] == exp)
             #######################################
-            print('\t\t'+nName + '=' +str(exp))   #
+            # print('\t\t'+nName + '=' +str(exp))   #
             #######################################
 
     return And(symbolic_states)
@@ -103,102 +103,10 @@ def my_symbolic_execution(dnn):
     ##########################################
     return temp
 
-def test(name='test'):
-    print('\n\n=============RUNNING:==========')
-    print('============='+name+'=============')
-    def get_dnn():
-        # (weights, bias, use activation function relu or not)
-        n00 = ([1.0, -1.0], 0.0, True)
-        n01 = ([1.0, 1.0], 0.0, True)
-        hidden_layer0 = [n00,n01]
-        n10 = ([0.5, -0.2], 0.0, True)
-        n11 = ([-0.5, 0.1], 0.0, True)
-        hidden_layer1 = [n10, n11]
-        # don't use relu for outputs
-        o0 = ([1.0, -1.0], 0.0, False)
-        o1 = ([-1.0, 1.0], 0.0, False)
-        output_layer = [o0, o1]
-        this_dnn = [hidden_layer0, hidden_layer1, output_layer]
-        return this_dnn
 
 
-    symbolic_states = my_symbolic_execution(get_dnn())
-
-    print("1. Generating random inputs and obtain outputs")
-    solve(symbolic_states)
-
-    print("2. Simultation concrete execution")
-    g = z3.And([i0 == 1.0, i1 == -1.0])
-    solve(z3.And(symbolic_states, z3.And(g)))
-
-    print("3. Checking assertions")
-    #1
-    g = z3.Implies(z3.And([n00 > 0.0, n01 == 0.0]), o0 > o1)
-    print(g)
-    prove(z3.Implies(symbolic_states, g))
-    #2
-    g = z3.Implies(z3.And([i0 - i1 > 0.0, i0 + i1 <= 0.0]), o0 > o1)
-    print(g)
-    prove(z3.Implies(symbolic_states, g))
-    #3
-    g = z3.Implies(i0 - i1 > 0.0, o0 > o1)
-    print(g)
-    prove(z3.Implies(symbolic_states, g))
-    print('============='+name+'=============')
-    print('=============DONE:============')
-
-def test2(name='test2'):
-    print('\n\n=============RUNNING:==========')
-    print('============='+name+'=============')
-    def get_dnn():
-        # (weights, bias, use activation function relu or not)
-        n00 = ([1.0, 1.0], 0.1, True)
-        n01 = ([0.5, 0.5], 0.2, True)
-        n02 = ([1.5, 0.0], 0.3, True)
-        n03 = ([0.0, 1.5], 0.4, True)
-        hidden_layer0 = [n00,n01,n02,n03]
-        n10 = ([0.5, -0.5, 0.0, 1.0], 1.0, True)
-        n11 = ([-0.5, 0.1, 2.0, -3.0], -2.0, True)
-        n12 = ([0.5, -0.2, 0.4, 1.0], 3.0, True)
-        n13 = ([-0.5, 0.1, 1.2, -0.5], -4.0, True)
-        hidden_layer1 = [n10, n11,n12,n13]
-        n20 = ([0.5, -0.2, 0.5, 0.5], -1.0, True)
-        n21 = ([-0.5, 0.1, 1.1, -1.0], 1.0, True)
-        n22 = ([0.5, -0.2, -0.5, 0.2], 1.0, True)
-        n23 = ([-0.5, 0.1, 1.1, 2.0], -1.0, True)
-        hidden_layer2 = [n20,n21,n22,n23]
-        # don't use relu for outputs
-        o0 = ([0.5, 0.5, -0.4, 1.0], 1.0, False)
-        o1 = ([-0.2, -0.5, 1.1, 2.0], 1.0, False)
-        output_layer = [o0, o1]
-        this_dnn = [hidden_layer0, hidden_layer1, hidden_layer2, output_layer]
-        return this_dnn
 
 
-    symbolic_states = my_symbolic_execution(get_dnn())
-
-    print("1. Generating random inputs and obtain outputs")
-    solve(symbolic_states)
-
-    print("2. Simultation concrete execution")
-    g = z3.And([i0 == 9, i1 == -10])
-    solve(And(symbolic_states, And(g)))
-
-    print("3. Checking assertions")
-    #1
-    g = Implies(And([i0 > 0.0, i1 > 0.0]), And([o0>0,  o1>0]))
-    print(g)
-    prove(z3.Implies(symbolic_states, g))
-    #2
-    g = Implies(And([i0==i1,i0<0]), And([o0!=0, o1!=0]))
-    print(g)
-    prove(z3.Implies(symbolic_states, g))
-    #3
-    g = Implies(i0 == i1 , o0 == o1)
-    print(g)
-    prove(Implies(symbolic_states, g))
-    print('============='+name+'=============')
-    print('=============DONE:============')
 
 if __name__ == '__main__':
     exec(open(argv[1]).read())
